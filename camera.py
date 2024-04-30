bc210196c1e7d7f757e00e12d6c2a9d02872bda5
bernard-li1 <bernard_li@brown.edu>
2024-04-30 17:37:30 -0400

    fnished user input

--- camera.py
+++ camera.py
@@ -4,17 +4,19 @@ from utils import overlay_image
 import numpy as np
 import random
 import cv2
+import sys
 
 rgb = cv2.VideoCapture(0)
 facec = cv2.CascadeClassifier('misc/haarcascade_frontalface_default.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-emotion_pairs = [("Angry", "data/images/angry.png"), ("Disgust", "data/images/disgust.png"),
-                 ("Fear", "data/images/fear.png"), ("Happy", "data/images/happy.png"),
-                 ("Sad", "data/images/sad.png"), ("Surprise", "data/images/surprise.png"),
-                 ("Neutral", "data/images/neutral.png")]
+emotion_pairs = {"angry" :"data/images/angry.png", "disgust": "data/images/disgust.png",
+                 "fear": "data/images/fear.png", "happy": "data/images/happy.png",
+                 "sad": "data/images/sad.png", "surprise": "data/images/surprise.png",
+                 "neutral": "data/images/neutral.png"}
 
 checkmark_file = "data/images/checkmark.png"
+end_screen_file = "data/images/lock.png"
 
 rect_width = 200
 rect_height = 50
@@ -39,9 +41,9 @@ def __get_data__():
     
     return faces, fr, gray
 
-def get_next_emotion():
-    emotion, emoji = random.choice(emotion_pairs)
-    return (emotion, emoji)
+def get_next_emotion(pwd_list):
+    emotion = pwd_list.pop(0)
+    return (emotion, emotion_pairs[emotion])
 
 def time_stall(emotion, emoji, checkmark, start_time, seconds):
     """
@@ -62,15 +64,43 @@ def time_stall(emotion, emoji, checkmark, start_time, seconds):
         cv2.imshow('Filter', fr)
         cv2.waitKey(2)
 
+def time_stall_end(end_screen, start_time, seconds, position=(0, 128)):
+    while (time.time() < start_time + seconds):
+        _, fr = rgb.read()
+        fr = overlay_image(fr, end_screen, position)
+        cv2.imshow('Filter', fr)
+        cv2.waitKey(2)
+
 def start_app(cnn):
     checkmark = cv2.imread(checkmark_file, -1)
-    next_emotion, emoji_file = random.choice(emotion_pairs)
+    end_screen = cv2.imread(end_screen_file, 1)
     detected_emotion = None
     start_time = None
     skip_frame = 10
     data = []
     flag = False
     ix = 0
+
+    while True:
+        user_input = input("Set Password (Angry, Disgust, Fear, Happy, Sad, Surprise, Neutral): ").split(",")
+        emotions = ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"]
+
+        password = []
+        flag = False
+
+        for emotion in user_input:
+            emotion = emotion.strip().lower()
+            if emotion not in emotions:
+                print("Please enter valid emotion(s): Angry, Disgust, Fear, Happy, Sad, Surprise, Neutral")
+                flag = True
+                break
+            else:   
+                password.append(emotion)
+        if flag:
+            continue
+        break
+    
+    next_emotion, emoji_file = get_next_emotion(password)
 
     while True:
         ix += 1
@@ -91,7 +121,7 @@ def start_app(cnn):
             
             # region of interest
             roi = cv2.resize(fc, (48, 48))
-            detected_emotion = cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            detected_emotion = cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis]).lower()
 
             cv2.putText(fr, detected_emotion, (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
@@ -108,7 +138,17 @@ def start_app(cnn):
             cv2.imshow('Filter', fr)
 
             time_stall(next_emotion, emoji, checkmark, start_time, sleep_secs)
-            next_emotion, emoji_file = random.choice(emotion_pairs)
+            
+            if password:
+                next_emotion, emoji_file = get_next_emotion(password)
+            else:
+                fr = overlay_image(fr, end_screen, position=(0, 128))
+                cv2.imshow('Filter', fr)
+                start_time = time.time()
+                time_stall_end(end_screen, start_time, sleep_secs, position=(0, 128))
+                
+                break
+
 
 
     cv2.destroyAllWindows()
